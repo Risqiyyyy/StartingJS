36638b89506025312d4f7cee533502bd0df519f2
Risqi Andryana S <109847964+Risqiyyyy@users.noreply.github.com>
2023-09-01 11:09:24 +0700

    fix bug database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,8 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
  
-const db = new Sequelize('<dbname>','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
+const db = new Sequelize('starting', 'root', '', {
+    host: "localhost",
+    dialect: "mysql"
 });
  
 export default db;
