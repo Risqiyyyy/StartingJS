061db804a2ebc2d5279f1fee1132560b3b4de6b3
Risqi Andryana S <109847964+Risqiyyyy@users.noreply.github.com>
2023-09-01 11:31:01 +0700

    fix frontend ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
+  const [auth, setAuth] = useState(false);
+  const location = useLocation();
+
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+      <CssBaseline />
+      <Routes>
+        <Route path="/login" element={<Login setAuth={setAuth} />} />
+        <Route path="/signup" element={<Signup setAuth={setAuth} />} />
+        <Route
+          path="/"
+          element={
+            auth ? (
+              <Home setAuth={setAuth} />
+            ) : (
+              <Navigate to="/login" state={{ from: location }} replace />
+            )
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
